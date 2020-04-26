b1b2d61f44e3833385b32192a69d66d585764f32
waseem <waseemmedhatg@gmail.com>
2020-04-27 01:04:34 +0200

    built ui elements with dummy content

--- app.R
+++ app.R
@@ -1,10 +1,32 @@
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
-  
+  titlePanel("Simple Linear Regression on the Iris dataset"),
+  sidebarLayout(
+    
+    sidebarPanel(
+      selectInput("y", "Dependent/response variable", choices = 1),
+      selectInput("x", "Inependent/explanatory variable", choices = 1)
+    ),
+    
+    mainPanel(
+      plotOutput("scatter"),
+      textOutput("summary")
+    )
+    
+  )
 )
 
 server <- function(input, output) {
+  
+  output$scatter <- renderPlot({
+    ggplot()
+  })
+  
+  output$summary <- renderText({
+    "Hello, friend."
+  })
   
 }
 
